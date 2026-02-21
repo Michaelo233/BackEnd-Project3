b59aa5461920919b7420af316187ba4d3752cf76
Michael Obikwere <oobikwere@academic.rrc.ca>
2026-02-21 16:32:51 -0600

    Feat: Added routes for get all events.

--- src/api/v1/routes/eventRoutes.ts
+++ src/api/v1/routes/eventRoutes.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import { validateRequest } from "../middleWare/validate";
-import * as postController from "../controllers/eventControllers";
+import * as eventController from "../controllers/eventControllers";
 import { eventSchemas } from "../validation/eventSchemas";
 
 const router = express.Router();
 
-router.post("/", validateRequest(eventSchemas.create), postController.createEventHandler);
-// router.get("/", postController.getAllPostsHandler);
+router.post("/", validateRequest(eventSchemas.create), eventController.createEventHandler);
+router.get("/", eventController.getAllEventsHandler);
 // router.get("/:id", validateRequest(postSchemas.getById), postController.getPostByIdHandler);
 // router.put("/:id", validateRequest(postSchemas.update), postController.updatePostHandler);
 // router.delete("/:id", validateRequest(postSchemas.delete), postController.deletePostHandler);
