1ed3fa87cad0f3d108b4cdcc6bde2b40b9d7853a
Michael Obikwere <oobikwere@academic.rrc.ca>
2026-02-21 16:32:21 -0600

    Feat: Added controller for get all events service.

--- src/api/v1/controllers/eventControllers.ts
+++ src/api/v1/controllers/eventControllers.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import * as postService from "../services/eventService";
+import * as eventService from "../services/eventService";
 import { successResponse } from "../models/responseModel";
 import { HTTP_STATUS } from "../../../constants/httpConstants";
 
@@ -13,9 +13,24 @@ export const createEventHandler = async (
         const {name, date, capacity, resgistrationCount, status, category} = req.body;
         const eventData = {name, date, capacity, resgistrationCount, status, category};
 
-        const newEvent = await postService.createEvent(eventData);
+        const newEvent = await eventService.createEvent(eventData);
 
-        res.status(HTTP_STATUS.OK).json(successResponse({newEvent}, "Post created successfully"));
+        res.status(HTTP_STATUS.OK).json(successResponse({newEvent}, "Event created successfully"));
+    } catch (error: unknown) {
+        next(error);
+    }
+};
+
+// handles GET request to read all events
+export const getAllEventsHandler = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    try {
+        const events = await eventService.getAllEvents();
+
+        res.status(HTTP_STATUS.OK).json(successResponse({events}, "Events retrieved successfully"));
     } catch (error: unknown) {
         next(error);
     }
