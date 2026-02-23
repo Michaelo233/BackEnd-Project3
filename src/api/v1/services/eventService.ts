df77b30c085dadb98d0d9a5bf6ea7f82533d106b
Michael Obikwere <oobikwere@academic.rrc.ca>
2026-02-22 19:21:07 -0600

    Feat: Added event service for update event function.

--- src/api/v1/services/eventService.ts
+++ src/api/v1/services/eventService.ts
@@ -82,3 +82,65 @@ export const getEventById = async (id: string): Promise<Event> => {
     }
 };
 
+// updating an existing event - userId or content
+export const updateEvent = async (id: string, 
+    eventData:{
+        name?: string, 
+        date?: string, 
+        capacity?: number
+        resgistrationCount?: number,
+        status?: string,
+        category?: string
+    }): Promise<Event | null> => {
+    try {
+        const updateEventData: Partial<Event> = {};
+        
+        if(eventData.name != undefined) {
+            updateEventData.name = eventData.name;
+        }
+
+        if(eventData.date != undefined) {
+            updateEventData.date = eventData.date;
+        }
+
+        if(eventData.capacity != undefined) {
+            updateEventData.capacity = eventData.capacity;
+        }
+
+        if(eventData.resgistrationCount != undefined) {
+            updateEventData.resgistrationCount = eventData.resgistrationCount;
+        }
+
+        if(eventData.status != undefined) {
+            updateEventData.status = eventData.status;
+        }
+        
+        if(eventData.category != undefined) {
+            updateEventData.category = eventData.category;
+        }
+
+        if(Object.keys(updateEventData).length === 0){
+            throw new Error("no fields provied to updated");
+        }        
+        
+        updateEventData.updatedAt = new Date().toISOString(); 
+
+        await firestoreRepository.updateDocument(COLLECTION, id, updateEventData);
+
+        const updatedEvent = await firestoreRepository.getDocById<Event>(COLLECTION, id);
+
+        if(!updatedEvent){
+            throw new Error("Updated event not found");
+        }
+
+        return updatedEvent;
+    } catch (error: unknown) {
+        const errorMessage =
+            error instanceof Error ? error.message : "Unknown error";
+        throw new Error(
+            `Failed to update the event: ${errorMessage}`
+        );
+    }
+};
+
+
