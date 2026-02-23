0b7fbfdabc8982a6f86b214245d31aa68c038f82
Michael Obikwere <oobikwere@academic.rrc.ca>
2026-02-22 19:16:37 -0600

    Feat: Added event service for get event by id.

--- src/api/v1/services/eventService.ts
+++ src/api/v1/services/eventService.ts
@@ -7,7 +7,7 @@ const COLLECTION = "events";
 
 // creating new event 
 export const createEvent = async (
-    postData: {
+    eventData: {
         name: string, 
         date: string, 
         capacity: number
@@ -21,20 +21,20 @@ export const createEvent = async (
         const events = await firestoreRepository.getAllDocuments<Event>(COLLECTION)
         const newEventData = {
             id: "evt_00000" + (events.length + 1).toString(), 
-            name: postData.name,
-            date: postData.date,
-            capacity: postData.capacity,
-            resgistrationCount: postData.resgistrationCount ?? 0,
-            status: postData.status ?? "active",
-            category: postData.category ?? "general",
+            name: eventData.name,
+            date: eventData.date,
+            capacity: eventData.capacity,
+            resgistrationCount: eventData.resgistrationCount ?? 0,
+            status: eventData.status ?? "active",
+            category: eventData.category ?? "general",
             createdAt: new Date().toISOString(),
             updatedAt: new Date().toISOString(),
         };
         
-        const event = await firestoreRepository.createDocument<Event>(COLLECTION, newEventData);
+        const eventId = await firestoreRepository.createDocument<Event>(COLLECTION, newEventData);
         
-        return {event, ... newEventData} as Event;
-
+        return {eventId, ... newEventData} as Event;
+        
     } catch (error: unknown) {
         const errorMessage =
             error instanceof Error ? error.message : "Unknown error";
@@ -47,8 +47,8 @@ export const createEvent = async (
 // to get all event in a collection 
 export const getAllEvents = async (): Promise<Event[]> => {
     try {
-        
-        const events = await firestoreRepository.getAllDocuments<Event>(COLLECTION)
+        // const event = await firestoreRepository.createDocument<Event>(COLLECTION, newEventData);
+        const events = await firestoreRepository.getAllDocuments<Event>(COLLECTION);
 
         return events;
 
@@ -60,3 +60,25 @@ export const getAllEvents = async (): Promise<Event[]> => {
         );
     }
 };
+
+// to find an existing event by id
+export const getEventById = async (id: string): Promise<Event> => {
+    try {
+        const event = await firestoreRepository.getDocById<Event>(COLLECTION, id);
+
+        if(!event){
+            throw new Error("Event not found");
+        } else {
+            return event;
+        }
+
+
+    } catch (error: unknown) {
+        const errorMessage =
+            error instanceof Error ? error.message : "Unknown error";
+        throw new Error(
+            `Failed to retrieve the event: ${errorMessage}`
+        );
+    }
+};
+
